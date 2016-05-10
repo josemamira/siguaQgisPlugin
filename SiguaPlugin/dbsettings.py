db01c43bcc4b710f0c9ef75efd7c9eea6ee3990a
josemamira <josema.mira@gmail.com>
2016-05-10 21:20:08 +0200

    Delete dbsettings.py

--- SiguaPlugin/dbsettings.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-__version__ = '1.0.0'
-VERSION = tuple(map(int, __version__.split('.')))
-
-# Edit this line to set your PostgreSQL connection
-params = ['<your host>', "5432",'<your database>', '<username>', '<password>']
