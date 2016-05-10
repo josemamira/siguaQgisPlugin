3e26b5424519c1fd330294b11c12265fd1604a92
josemamira <josema.mira@gmail.com>
2016-05-10 10:20:06 +0200

    Update dbsettings.py

--- SiguaPlugin/dbsettings.py
+++ SiguaPlugin/dbsettings.py
@@ -4,5 +4,6 @@
 __version__ = '1.0.0'
 VERSION = tuple(map(int, __version__.split('.')))
 
+# Edit this line to set your PostgreSQL connection
 params = ['<your host>', "5432",'<your database>', '<username>', '<password>']
 
