745ddd94aba2ad2fd624f537b7473e7c9fc050dc
josemamira <josema.mira@gmail.com>
2016-05-10 10:19:15 +0200

    Update dbsettings.py

--- SiguaPlugin/dbsettings.py
+++ SiguaPlugin/dbsettings.py
@@ -4,7 +4,5 @@
 __version__ = '1.0.0'
 VERSION = tuple(map(int, __version__.split('.')))
 
-#params = ['172.25.224.197', "5432",'sigua', 'jose', 'j3mlabsig']
-params = ['172.25.224.197', "5432",'sigua', 'espacios', 'siguadmin']
-#params = ['193.145.231.168', "5432",'sigua', 'espacios', 'siguadmin']
-#params = ['localhost', "5432",'sigua', 'jose', 'j3m']
+params = ['<your host>', "5432",'<your database>', '<username>', '<password>']
+
